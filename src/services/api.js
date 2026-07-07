75f697ad38646ebb7271bc9e37472af4d17d5781
losfames <felipep21silva@gmail.com>
2026-07-06 21:19:52 -0300

    aaa 2

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Configura o endereço base da sua API em C#
 const api = axios.create({
-    baseURL: 'http://localhost:5275/api', // Confira se a porta do seu C# no Visual Studio é 5000
+    baseURL: 'https://backconsultafinance-production.up.railway.app/api', // Confira se a porta do seu C# no Visual Studio é 5000
 });
 
 // Interceptor: Adiciona automaticamente o Token JWT em TODAS as requisições
