1c75610161e13956e02b32a62b15bfb05ccfa767
losfames <felipep21silva@gmail.com>
2026-09-10 12:43:17 -0300

    Corrige CORS e URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,17 @@
-// src/services/api.js
 import axios from 'axios';
 
-// Configura o endereço base da sua API em C#
 const api = axios.create({
-    baseURL: 'https://backconsulta-finance.onrender.com', // Confira se a porta do seu C# no Visual Studio é 5000
+    baseURL: 'https://backconsulta-finance.onrender.com/api',
 });
 
-// Interceptor: Adiciona automaticamente o Token JWT em TODAS as requisições
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('consultaFinance:token');
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
 
-export default api;// JavaScript source code
+export default api;
